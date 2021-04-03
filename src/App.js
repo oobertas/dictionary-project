c6b7c263c05550740b96232a4436f3a5c1ea0fc1
oobertas <65638737+oobertas@users.noreply.github.com>
2021-04-03 16:22:45 -0400

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary/Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center app-footer">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/olha-obertas-77932b15b/"
+            target="_blank"
+          >
+            Olha Obertas
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/oobertas" target="_blank">
+            open-sourced on GitHub
+          </a>
+          <div>
+            Icons made by{" "}
+            <a href="https://www.freepik.com" title="Freepik" target="_blank">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a
+              href="https://www.flaticon.com/"
+              title="Flaticon"
+              target="_blank"
+            >
+              {" "}
+              www.flaticon.com
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../Dictionary/Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeyWordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus="true" onChange={handleKeyWordChange} />
+      </form>
+    </div>
+  );
+}
